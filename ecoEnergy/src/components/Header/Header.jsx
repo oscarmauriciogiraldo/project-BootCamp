d900df856a58f01ed443dede6047eff0a586c6d7
oscarmauriciogiraldo <oscardan@utp.edu.co>
2025-05-14 16:49:17 -0500

    navbar menu structure

---
+++ ecoEnergy/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavbarMenu = [
+    {
+        id: 1,
+        title: 'Home',
+        path: '/',
+    },
+]
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
